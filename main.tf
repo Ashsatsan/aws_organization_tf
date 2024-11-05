67ea524be6af6d331de21ac1f27bcb9902a3bb9c
ashsatsan <ahundal6@myseneca.ca>
2024-11-04 23:36:06 -0500

    commiting changes

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
-    backend "s3" {
-       bucket = "flow-tf-unitou"
-       key = "terraform_ou.tf"
-       region = "us-east-2"
-    }
+  backend "s3" {
+    bucket = "flow-tf-unitou"
+    key    = "terraform_ou.tf"
+    region = "us-east-2"
+  }
 }
 
 provider "random" {}
 
 provider "aws" {
-  region = "us-east-2"  # Replace with your preferred AWS region
+  region = "us-east-2" # Replace with your preferred AWS region
 }
 
 provider "aws" {
-  alias = "central"
+  alias  = "central"
   region = "us-west-1"
 }
 
