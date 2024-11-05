73431868010cc1321968d32b94551a342ba1754b
ashsatsan <ahundal6@myseneca.ca>
2024-11-05 00:30:56 -0500

    commiting nges

--- accounts.tf
+++ accounts.tf
@@ -1,8 +1,8 @@
 resource "aws_organizations_account" "dev_account" {
-  name         = "DevAccount"  # Use 'name' instead of 'account_name'
-  email        = "6qq7p@livinitlarge.net"  # Ensure this email is unique
-  role_name    = "OrganizationAccountAccessRole"
-  parent_id    = aws_organizations_organizational_unit.dev.id  # Reference directly
+  name      = "DevAccount"             # Use 'name' instead of 'account_name'
+  email     = "6qq7p@livinitlarge.net" # Ensure this email is unique
+  role_name = "OrganizationAccountAccessRole"
+  parent_id = aws_organizations_organizational_unit.dev.id # Reference directly
 
   tags = {
     Name = "Development Account"
@@ -10,10 +10,10 @@ resource "aws_organizations_account" "dev_account" {
 }
 
 resource "aws_organizations_account" "test_account" {
-  name         = "TestAccount"  # Use 'name' instead of 'account_name'
-  email        = "6qq7p@livinitlarge.net"  # Ensure this email is unique
-  role_name    = "OrganizationAccountAccessRole"
-  parent_id    = aws_organizations_organizational_unit.test.id  # Reference directly
+  name      = "TestAccount"            # Use 'name' instead of 'account_name'
+  email     = "6qq7p@livinitlarge.net" # Ensure this email is unique
+  role_name = "OrganizationAccountAccessRole"
+  parent_id = aws_organizations_organizational_unit.test.id # Reference directly
 
   tags = {
     Name = "Testing Account"
