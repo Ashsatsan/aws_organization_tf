f5bd9862189a73e423c54edf27900195a5c0ab66
ashsatsan <ahundal6@myseneca.ca>
2024-11-04 23:19:17 -0500

    commiting changes

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+    backend "s3" {
+       bucket = "flow-tf-unitou"
+       key = "terraform_ou.tf"
+       region = "us-east-2"
+    }
+}
+
 provider "random" {}
 
 provider "aws" {
@@ -9,32 +17,3 @@ provider "aws" {
   region = "us-west-1"
 }
 
-# S3 bucket for storing Terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "not_require_just_testing_3848"  # Replace with a globally unique bucket name
-
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Dev"
-  }
-}
-
-# DynamoDB table for state locking
-resource "aws_dynamodb_table" "example" {
-  name           = "db_tf_org"
-  billing_mode   = "PROVISIONED"
-  hash_key       = "Id"  # Specify the attribute that holds the hash key
-
-  attribute {
-    name = "Id"
-    type = "S"
-  }
-
-
-
-  # Additional configuration like range_key, etc.
-}
