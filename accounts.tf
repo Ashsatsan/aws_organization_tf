c1af27c6fac44499248aa377f8f976305b4b010f
ashsatsan <ahundal6@myseneca.ca>
2024-11-05 00:28:31 -0500

    commiting changes

--- accounts.tf
+++ accounts.tf
@@ -1,8 +1,8 @@
 resource "aws_organizations_account" "dev_account" {
-  account_name  = "DevAccount"
-  email         = "devaccount@example.com"  # Ensure this email is unique
-  role_name     = "OrganizationAccountAccessRole"
-  parent_id     = aws_organizations_organizational_unit.dev.id  # Reference directly
+  account_name = "DevAccount"
+  email        = "devaccount@example.com" # Ensure this email is unique
+  role_name    = "OrganizationAccountAccessRole"
+  parent_id    = aws_organizations_organizational_unit.dev.id # Reference directly
 
   tags = {
     Name = "Development Account"
@@ -11,10 +11,10 @@ resource "aws_organizations_account" "dev_account" {
 }
 
 resource "aws_organizations_account" "test_account" {
-  account_name  = "TestAccount"
-  email         = "6qq7p@livinitlarge.net"  # Ensure this email is unique
-  role_name     = "OrganizationAccountAccessRole"
-  parent_id     = aws_organizations_organizational_unit.test.id  # Reference directly
+  account_name = "TestAccount"
+  email        = "6qq7p@livinitlarge.net" # Ensure this email is unique
+  role_name    = "OrganizationAccountAccessRole"
+  parent_id    = aws_organizations_organizational_unit.test.id # Reference directly
 
   tags = {
     Name = "Testing Account"
